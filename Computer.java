063eab1e240c535dd9ec8f6f28e954c0414b1ad8
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:32:29 -0700

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,5 +1,39 @@
 package part1;
 
 public class Computer implements ComputerInterface {
+  
+  private Adder adder;
+	
+	private Complementer complementer;
+	
+	private Bus bus;
+	
+	private Printer printer;
+	
+	private Reader reader;
+	
+	public Computer() {
+		
+	
+	}
+	
+	public void build() {
+		
+		Register ra;
+		
+		Register rb;
+		
+		Register rc;
+		
+		Register rd;
+		
+	}
+	
+	public void run() {
+		
+		
+	}
+
+}
 
 }
