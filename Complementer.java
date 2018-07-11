7a79bf251b611ef942432750147b4d0a00faf934
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:26:30 -0700

    Update Complementer.java

--- Complementer.java
+++ Complementer.java
@@ -1,5 +1,15 @@
 package part1;
 
 public class Complementer implements ComplementerInterface {
+  
+  public Complementer( ) {
+		
+	}
+	
+	public void changeSign( int a) {
+		
+		a = ( (-1) * a);
+	
+	}
 
 }
