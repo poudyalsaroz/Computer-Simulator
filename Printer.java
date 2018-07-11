193bd82b9560e2481f44023cd3c4bb1cd6d27838
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:33:31 -0700

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -2,4 +2,15 @@ package part1;
 
 public class Printer implements PrinterInterface {
 	
+	public Printer() {
+		
+	}
+	
+	public void printInteger(int a) {
+		
+		System.out.println (">> a");
+	}
+
+}
+	
 }
