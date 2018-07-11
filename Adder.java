88b07176988944ab3d81a03351edb701240b8ae0
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:20:36 -0700

    Update Adder.java

--- Adder.java
+++ Adder.java
@@ -1,5 +1,31 @@
 package part1;
 
 public class Adder implements AdderInterface {
+  
+  private int a;
+	
+	private int b;
+	
+	private int sum;
+	
+	public Adder( int x, int y ) {
+		
+		a = x;
+		
+		b = y;
+	
+	}
+	
+	public void add() {
+		
+		sum = a + b;
+		
+	}
+	
+	public int getSum() {
+		
+		return sum;
+		
+	}
 
 }
