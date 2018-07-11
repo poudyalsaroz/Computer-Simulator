2c7c5fd582aadcb04492dde8ccb31c31391aeeab
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:21:39 -0700

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -1,5 +1,21 @@
 package part1;
 
 public class Bus implements BusInterface {
+  
+  private int integer;
+	
+	public void setInteger (int a ) {
+		
+		integer = a;
+	
+	}
+	
+	public int getInteger() {
+		
+		return integer;
+	
+	}
+	
+
 
 }
