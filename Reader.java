8c7151a48b7bd41f911469c32ce95623a2f3e190
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:35:44 -0700

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,5 +1,26 @@
 package part1;
 
 public class Reader implements ReaderInterface {
+  
+  private int a;
+	
+	public Reader() {
+		
+	}
+	
+	public void storeInteger() throws IOException {	
+	
+	System.out.println(" Enter the integer: ");
+	
+	BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+	
+	String str = br.readLine();
+	
+	a = Integer.parseInt(str);
+	
+	System .out.print("* ");
+	
+	}
+
 
 }
